fb46d972c900a6d7e3d07d8ba1285f5e2acabfb0
zhy756303268-afk <zhy756303268@gmail.com>
2025-08-26 20:43:19 +0800

    Update generic.py

--- api/servers/generic.py
+++ api/servers/generic.py
@@ -13,8 +13,7 @@ PLATFORM_API_URLS: Dict[str, str] = {
     "groq": "https://api.groq.com/openai/v1/chat/completions",
     "cerebras": "https://api.cerebras.ai/v1/chat/completions",
     "nvidia": "https://integrate.api.nvidia.com/v1/chat/completions",
-    "sambanova": "https://api.sambanova.ai/v1/chat/completions",
-}
+    "sambanova": "AIzaSyC_B6OV-lZ3fPT2SwAYAFdOGo-ZV94Ij6c","AIzaSyA8XcFL_Dm9BA3hXXh3f5etLowy6PHOrqE",
 
 
 @router.post("/{platform}/chat/completions")
